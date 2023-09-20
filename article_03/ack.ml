ae77f1c6effa9b4dd3d6314fcd49b0e419e9cb47
Pavel Tisnovsky <ptisnovs@redhat.com>
2023-09-20 08:58:30 +0200

    Ackermann function

---
+++ article_03/ack.ml
@@ -0,0 +1,7 @@
+let rec ackermann = function
+  | m,n when m < 0 || n < 0 -> invalid_arg "Ackermann's function is only defined over the non-negative integers"
+  | 0,n -> n+1
+  | m,0 -> ackermann (m-1,1)
+  | m,n -> ackermann (m-1,ackermann (m,n-1));;
+
+ackermann (2,10)
