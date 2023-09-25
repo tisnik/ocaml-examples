36e879ae7cedce21a5417a626c69b20a57160318
Pavel Tisnovsky <ptisnovs@redhat.com>
2023-09-25 08:00:31 +0200

    Pattern matching, 5th example

---
+++ article_03/pattern5.ml
@@ -0,0 +1,9 @@
+let rec ackermann m n =
+  match m, n with
+  | (0,n) -> n+1
+  | (m,0) -> ackermann (m-1) 1
+  | (m,n) -> ackermann (m-1) (ackermann m (n-1));;
+
+
+Printf.printf "%d" (ackermann 2 10);;
+Printf.printf "%d" (ackermann -2 10);;
