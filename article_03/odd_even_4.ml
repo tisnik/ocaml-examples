6283cf276acb0f00c45d941ed716e4e08a392924
Pavel Tisnovsky <ptisnovs@redhat.com>
2023-09-25 08:00:31 +0200

    Mutual recursion, 4th variant

---
+++ article_03/odd_even_4.ml
@@ -0,0 +1,14 @@
+let even x =
+  if x = 0
+  then true
+  else odd (x-1)
+and odd x =
+  if x = 0
+  then false
+  else even (x-1);;
+
+
+Printf.printf "%b" (even 1);;
+Printf.printf "%b" (even 2);;
+Printf.printf "%b" (even 3);;
+Printf.printf "%b" (even 4);;
