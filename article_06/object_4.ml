1c33cad39c781fbc06e7eb1438a69b4590efc98c
Pavel Tisnovsky <ptisnovs@redhat.com>
2023-11-13 08:12:02 +0100

    OOP in OCaml: example #4

---
+++ article_06/object_4.ml
@@ -0,0 +1,19 @@
+class vector (x:int) (y:int) =
+  object (self)
+    val x = x
+    val y = y
+    method print = Printf.printf "Vector: %dx%d\n" x y
+    method get_x = x
+    method get_y = y
+  end;;
+
+let(+) (a: vector) (b: vector) = new vector (a#get_x+b#get_x) (a#get_y+b#get_y);;
+
+let v1 = new vector 10 20;;
+v1#print;;
+
+let v2 = new vector 1 2;;
+v2#print;;
+
+let v3 = v1 + v2;;
+v3#print;;
