c91c52a2fbe9f1f79eb880c703375dcd88840832
Pavel Tisnovsky <ptisnovs@redhat.com>
2023-12-25 09:04:02 +0100

    if-elseif-else with nested block

---
+++ article_08/pos_neg_zero_3.ml
@@ -0,0 +1,14 @@
+let classify x =
+  if x > 0 then "positive"
+  else if x < 0 then "negative"
+  else begin
+    print_string "Zero value detected";
+    "zero"
+  end
+;;
+
+classify (-10);;
+classify (-1);;
+classify 0;;
+classify 1;;
+classify 10;;
